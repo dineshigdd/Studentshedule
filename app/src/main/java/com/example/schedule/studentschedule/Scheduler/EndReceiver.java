31b12aa10ccd21e0039ae73952bdbc0aff4a597c
Dinesh Wickramasinghe <dineshigdd@gmail.com>
2018-11-21 10:46:03 -0800

    change EndReciver

--- app/src/main/java/com/example/schedule/studentschedule/Scheduler/EndReceiver.java
+++ app/src/main/java/com/example/schedule/studentschedule/Scheduler/EndReceiver.java
@@ -11,7 +11,7 @@ public class EndReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         // TODO: This method is called when the BroadcastReceiver is receiving
         // an Intent broadcast.
-       
+
         String date = intent.getSerializableExtra("END-DAY").toString();
         String course = intent.getSerializableExtra("COURSE").toString();
         EndNotificationScheduler.showNotification(context, date, course, EndNotificationScheduler.notificationID );
